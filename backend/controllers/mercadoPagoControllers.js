597ffa0bb4d2b14a0dc8691025d2cf206be7aa85
Magui Moraga <margaritamoraga.m22.com>
2025-06-25 23:34:25 -0700

    MercadoPagoControllersConfigure6

--- backend/controllers/mercadoPagoControllers.js
+++ backend/controllers/mercadoPagoControllers.js
@@ -1,7 +1,7 @@
 const mercadopago = require('mercadopago');
 const { sendOrderConfirmationToCompany, sendPaymentConfirmationToClient } = require('../pdf/controllers/emailController');
 
-mercadopago.configurations.setAccessToken(process.env.MP_ACCESS_TOKEN);
+mercadopago.access_token = process.env.MP_ACCESS_TOKEN;
 
 const crearSuscripcionDinamica = async (req, res) => {
   try {
