9fbf3cde1aae329b13d4a438a9d552057efc8abd
AlanRanchs <alanjrr.isc@gmail.com>
2025-07-02 18:03:20 -0600

    Se añade logica al boton de recuperar contraseña, a ver si jala esta cosa

--- backend/auth/routes/authRoutes.js
+++ backend/auth/routes/authRoutes.js
@@ -3,10 +3,15 @@ const router = express.Router();
 const { loginAdmin } = require('../authController/authController');
 const { verifyToken } = require('../middlewares/authMiddleware');
 const { registerAdmin } = require('../authController/authController');
+const { forgotPassword } = require('../authController/authController');
+const { resetPassword } = require('../authController/authController');
 
-// Ruta de login
+// Ruta de login, registro, recuperación y restablecimiento de contraseña
+// Estas rutas manejan las operaciones de autenticación del administrador
 router.post('/login', loginAdmin);
 router.post('/register', registerAdmin);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
 
 // Ruta protegida de ejemplo
 router.get('/verify', verifyToken, (req, res) => {
