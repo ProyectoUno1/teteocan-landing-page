67577281ed5145b7edc336b23a9113c54151163d
ProyectoUno1 <proyecto.landingpage@gmail.com>
2025-07-07 11:53:16 -0500

    Update cleanPendiente.js
    Cron job updated to be executed daily at 5:00 a.m.

--- backend/crondb/cleanPendiente.js
+++ backend/crondb/cleanPendiente.js
@@ -7,12 +7,12 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false }
 });
 
-cron.schedule('*/10 * * * *', async () => {
-  console.log('[CRON] Ejecutando limpieza automática cada 10 minutos...');
+cron.schedule('0 5 * * *', async () => {
+  console.log('[CRON] Ejecutando limpieza diaria...');
   try {
     await pool.query('CALL CleanPendiente();');
-    console.log('[CRON] Limpieza completada correctamente.');
+    console.log('[CRON] Limpieza diaria completada correctamente.');
   } catch (error) {
-    console.error('[CRON] Error al ejecutar limpieza:', error.message);
+    console.error('[CRON] Error al ejecutar limpieza diaria:', error.message);
   }
 });
